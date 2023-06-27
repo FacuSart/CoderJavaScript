9790b8a24d44e3bf9507859710d82fbf241db904
Facundo <facusartori03@gmail.com>
2023-06-27 10:36:41 -0300

    Segunda PreEntrega

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 let total = 0;
-
+let carrito = [];
 let opcion;
+let i = 0;
+let productos = [
+    {nombre:'Creatina', marca:'Nutrilab', precio: 900},{nombre:'Creatina', marca:'ENA', precio: 2000},{nombre:'Creatina', marca:'Xbody', precio: 1000},{nombre:'Creatina', marca:'AMZ', precio: 1500},
+    {nombre:'Proteina', marca:'ENA', precio: 1500},{nombre:'Proteina', marca:'Star Nutrition', precio: 2000},{nombre:'Proteina', marca:'Xbody', precio: 900},{nombre:'Proteina', marca:'ENA X-pro', precio: 1000}
+];
 
-const agregarCarrito = (precio) =>{
-    let cantidad = parseInt(prompt("Ingrese cantidad"));
+
+
+const agregarCarrito = (precio,cantidad) =>{
     if(cantidad > 0){
-        total = total + precio * cantidad;
+        total += precio * cantidad;
         alert("Total del carrito: " + total);
         return total;
     }else{
@@ -13,6 +19,21 @@ const agregarCarrito = (precio) =>{
     }
 }
 
+const itemsCarrito = (nombre, marca, precio, cantidad) =>{
+    carrito[i] = new Item(nombre, marca, precio, cantidad)
+    for(let j = 0; j<carrito.length;j++){
+
+        if(carrito[j].nombre == carrito[i].nombre && carrito[j].marca == carrito[i].marca && j !== i){
+            carrito[j].cantidad += carrito[i].cantidad
+            carrito.pop()
+            break;
+        }
+    }
+    i++;
+
+    console.log(carrito.length)
+    return carrito[i-1];
+}
 const vaciarCarrito = () =>{
     let respuesta;
     do{
@@ -20,6 +41,8 @@ const vaciarCarrito = () =>{
         switch (respuesta){
             case "si":
                 total = 0;
+                carrito = [];
+                i = 0;
                 alert( "Carrito vaciado" );
                 break;
             case "no":
@@ -32,60 +55,61 @@ const vaciarCarrito = () =>{
     }while(respuesta != "no" && respuesta != "si");
 }
 
-// const mostrarCarrito = () =>{
-//     alert("El total de su carrito es de: $"+total);
-// }
-
-alert("Bienvenido a la tienda")
+const mostrarCarrito = () =>{
+    if(total !== 0){
+        alert("El total de su carrito es de: $"+total);
+        carrito.forEach((item) => {alert("x"+item.cantidad+" "+item.nombre+" "+ item.marca +" $"+item.precio * item.cantidad)})
+    }else{
+        alert("Su carrito está vacío");
+    }
+    
+}
 
 const mostrarMenu = () =>{
 
-    opcion = parseInt(prompt("El total de su carrito es de: $"+total+ "\nQue operación desea realizar: \n 1_Comprar Producutos. \n 2_Vaciar Carrito."));
-
+    opcion = parseInt(prompt("El total de su carrito es de: $"+total+ "\nQue operación desea realizar: \n 1_Comprar Producutos. \n 2_Vaciar Carrito. \n 3_Mostrar Carrito."));
 
     switch (opcion){
         case 1:
             let continuar = "";
             do{
-                let opcionCompra = parseInt (prompt("Creatinas: \n 1_Creatina Nutrilab: $900 \n 2_Creatina Star Nutrition: $2000 \n 3_Creatina Xbody: $1000 \n 4_Creatina AMZ: $1500 \n Proteinas: \n 5_Proteina ENA: $1500 \n 6_Proteina Star Nutrition: $2000 \n 7_Proteina Xbody: $900 \n 8_Proteina ENA x-pro: $1000 \n "));
-                switch(opcionCompra){
-                    case 1:
-                        agregarCarrito(900);
-                        break;
-                    case 2:
-                        agregarCarrito(2000);
-                        break;
-                    case 3:
-                        agregarCarrito(1000);
-                        break;
-                    case 4:
-                        agregarCarrito(1500);
-                        break;
-                    case 5:
-                        agregarCarrito(1500);
-                        break;
-                    case 6:
-                        agregarCarrito(2000);
-                        break;
-                    case 7:
-                        agregarCarrito(900);
-                        break;
-                    case 8:
-                        agregarCarrito(1000);
-                        break;
-                    default:
-                        alert("Opción inválida, intente nuevamente...");
-                        break;
+                let opcionCompra = parseInt (prompt("Creatinas: \n 1_Creatina Nutrilab: $900 \n 2_Creatina ENA: $2000 \n 3_Creatina Xbody: $1000 \n 4_Creatina AMZ: $1500 \n Proteinas: \n 5_Proteina ENA: $1500 \n 6_Proteina Star Nutrition: $2000 \n 7_Proteina Xbody: $900 \n 8_Proteina ENA x-pro: $1000 \n "));
+                let cantidad = 0;
+                function definirCant (){
+                    cantidad = parseInt(prompt("Ingrese cantidad"))
                 }
-            continuar = prompt("Desea continuar comprando? (si/no)")
-            }while(continuar == "si")
+                while(0 < opcionCompra && opcionCompra <= productos.length){
+                    definirCant();
+                    agregarCarrito(productos[opcionCompra-1].precio, cantidad, itemsCarrito(productos[opcionCompra-1].nombre, productos[opcionCompra-1].marca, productos[opcionCompra-1].precio, cantidad))
+                    break;
+                }
+                if(opcionCompra <= 0 || opcionCompra >productos.length){
+                    alert("Opción no válida, intente nuevamente");
+                    mostrarMenu();
+                }
+                
+
+                do{
+                    continuar = prompt("Desea continuar comprando? (si/no)")
+                    if(continuar !== "no" && continuar !== "si"){
+                        alert("Opción no válida, intente nuevamente");
+                    }
+                }while(continuar !== "no" && continuar !== "si");
+
+            }while(continuar !== "no");
             break;
         case 2:
             vaciarCarrito();
+            break;     
+        case 3:
+            mostrarCarrito();
             break;    
-        // case 3:
-        //     mostrarCarrito();
-        //     break;       
+        default:
+            alert("Opción no válida, intente nuevamente");
+            mostrarMenu();
+            break;
+
     }
 }
+alert("Bienvenido a la tienda")
 mostrarMenu();
